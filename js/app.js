1a1fe5cefc2311b1835fa0be84f7b2914198da40
Nsuliman <nsuliman88@gmail.com>
2019-10-26 21:05:45 +0200

    done additem function app.js

--- js/app.js
+++ js/app.js
@@ -7,7 +7,9 @@ var Cart = function(items) {
 };
 
 Cart.prototype.addItem = function(product, quantity) {
-  // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  // Done: Fill in this instance method to create a new CartItem and add it to this.items
+  var itemnew = new CartItem(product,quantity);
+  this.items.push(itemnew);
 };
 
 Cart.prototype.saveToLocalStorage = function() {
