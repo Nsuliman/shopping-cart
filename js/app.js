30c00ad2846dd1e790b776d56d6f6b7ee8e32691
Nsuliman <nsuliman88@gmail.com>
2019-10-26 21:11:21 +0200

    done saveToLocalStorage function app.js

--- js/app.js
+++ js/app.js
@@ -14,6 +14,8 @@ Cart.prototype.addItem = function(product, quantity) {
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  var saveItemsStrg = JSON.stringify(this.items);
+  localStorage.setItem(' All-Items ' , saveItemsStrg);
 };
 
 Cart.prototype.removeItem = function(item) {
